fd9d6bc1152b2488158618a54621bb7685dd5d8d
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2026-05-02 14:07:52 +0300

    Arrangement suitable for Surya

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-add_lunch_combo omni_surya-user
-add_lunch_combo omni_surya-userdebug
-add_lunch_combo omni_surya-eng
+add_lunch_combo twrp_surya-user
+add_lunch_combo twrp_surya-userdebug
+add_lunch_combo twrp_surya-eng
