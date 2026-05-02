283ba181fceb273a160cea91806f1343c04f85d1
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2026-05-02 13:57:57 +0300

    Arrangement suitable for Surya
    Updated lunch choices to include TWRP variants.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,14 +1,7 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_surya-user \
-    omni_surya-userdebug \
-    omni_surya-eng
+    twrp_surya-user \
+    twrp_surya-userdebug \
+    twrp_surya-eng
