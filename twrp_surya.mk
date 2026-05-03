23a22e0bf4c9f5c3de3855d3cdd3692d838a7a59
oliplusmzr <96580492+oliplusmzr@users.noreply.github.com>
2026-05-03 14:04:43 +0300

    Update product inheritance in twrp_surya.mk

--- twrp_surya.mk
+++ twrp_surya.mk
@@ -1,16 +1,10 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
